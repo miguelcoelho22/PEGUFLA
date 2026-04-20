a4fb32d2b65cda73a525da74f4b8076ede40d56b
miguel_coelho <miguel.coelho@estudante.ufla.br>
2026-04-20 10:14:26 -0300

    criando repositorio de user

---
+++ backend/PEGUFLA/src/main/java/br/ufla/PEGUFLA/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package br.ufla.PEGUFLA.repository;
+
+import br.ufla.PEGUFLA.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByLogin(String login);
+}
