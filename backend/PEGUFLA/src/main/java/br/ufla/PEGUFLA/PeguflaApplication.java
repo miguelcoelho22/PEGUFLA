78d3ec9cd137539c8014481fde51e33adc05cabe
miguel_coelho <miguel.coelho@estudante.ufla.br>
2026-04-20 09:42:47 -0300

    Feat: configurando docker e dependencias maven

---
+++ backend/PEGUFLA/src/main/java/br/ufla/PEGUFLA/PeguflaApplication.java
@@ -0,0 +1,13 @@
+package br.ufla.PEGUFLA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeguflaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeguflaApplication.class, args);
+	}
+
+}
