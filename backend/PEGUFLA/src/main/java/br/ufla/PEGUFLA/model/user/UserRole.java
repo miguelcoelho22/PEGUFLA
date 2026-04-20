02a46f86cc3b54a8bbeb1b6932584bdff6d347e7
miguel_coelho <miguel.coelho@estudante.ufla.br>
2026-04-20 10:01:47 -0300

    criando a entidade user

---
+++ backend/PEGUFLA/src/main/java/br/ufla/PEGUFLA/model/user/UserRole.java
@@ -0,0 +1,15 @@
+package br.ufla.PEGUFLA.model.user;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private final String role;
+
+    UserRole(String role){
+        this.role = role;
+    }
+}
